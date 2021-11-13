592a0f6da2cb4cde42cd04c12ea625af0e972a63
Dimitrii Lopanov <sexykdi@gmail.com>
2021-11-13 17:07:08 +0300

    clarify name

--- streams.go
+++ streams.go
@@ -21,48 +21,48 @@ func (app *Application) StartStreams() {
 
 		hlsEnabled := typeExists("hls", supportedTypes)
 
-		go func(name uuid.UUID, hlsEnabled bool, url string) {
+		go func(streamID uuid.UUID, hlsEnabled bool, url string) {
 			for {
-				log.Printf("Stream must be establishment for '%s' by connecting to %s", name, url)
+				log.Printf("Stream must be establishment for '%s' by connecting to %s", streamID, url)
 				rtsp.DebugRtsp = false
 				session, err := rtsp.Dial(url)
 				if err != nil {
-					hlserror.SetError(name, 502, fmt.Errorf("rtsp.Dial error for %s (%s): %s", name, url, err.Error()))
-					log.Printf("rtsp.Dial error for %s (%s): %s\n", name, url, err.Error())
+					hlserror.SetError(streamID, 502, fmt.Errorf("rtsp.Dial error for %s (%s): %s", streamID, url, err.Error()))
+					log.Printf("rtsp.Dial error for %s (%s): %s\n", streamID, url, err.Error())
 					time.Sleep(60 * time.Second)
 					continue
 				}
 				session.RtpKeepAliveTimeout = time.Duration(10 * time.Second)
 				codec, err := session.Streams()
 				if err != nil {
-					hlserror.SetError(name, 520, fmt.Errorf("Can't get sessions for %s (%s): %s", name, url, err.Error()))
-					log.Printf("Can't get sessions for %s (%s): %s\n", name, url, err.Error())
+					hlserror.SetError(streamID, 520, fmt.Errorf("Can't get sessions for %s (%s): %s", streamID, url, err.Error()))
+					log.Printf("Can't get sessions for %s (%s): %s\n", streamID, url, err.Error())
 					time.Sleep(60 * time.Second)
 					continue
 				}
-				app.codecAdd(name, codec)
-				err = app.updateStatus(name, true)
+				app.codecAdd(streamID, codec)
+				err = app.updateStatus(streamID, true)
 				if err != nil {
-					log.Printf("Can't update status 'true' for %s (%s): %s\n", name, url, err.Error())
+					log.Printf("Can't update status 'true' for %s (%s): %s\n", streamID, url, err.Error())
 					time.Sleep(60 * time.Second)
 					continue
 				}
 
 				if hlsEnabled {
 					stopHlsCast := make(chan bool, 1)
-					app.startHlsCast(name, stopHlsCast)
+					app.startHlsCast(streamID, stopHlsCast)
 					for {
 						pkt, err := session.ReadPacket()
 						if err != nil {
-							hlserror.SetError(name, 500, fmt.Errorf("Can't read session's packet %s (%s): %s", name, url, err.Error()))
-							log.Printf("Can't read session's packet %s (%s): %s\n", name, url, err.Error())
+							hlserror.SetError(streamID, 500, fmt.Errorf("Can't read session's packet %s (%s): %s", streamID, url, err.Error()))
+							log.Printf("Can't read session's packet %s (%s): %s\n", streamID, url, err.Error())
 							stopHlsCast <- true
 							break
 						}
-						err = app.cast(name, pkt)
+						err = app.cast(streamID, pkt)
 						if err != nil {
-							hlserror.SetError(name, 500, fmt.Errorf("Can't cast packet %s (%s): %s", name, url, err.Error()))
-							log.Printf("Can't cast packet %s (%s): %s\n", name, url, err.Error())
+							hlserror.SetError(streamID, 500, fmt.Errorf("Can't cast packet %s (%s): %s", streamID, url, err.Error()))
+							log.Printf("Can't cast packet %s (%s): %s\n", streamID, url, err.Error())
 							stopHlsCast <- true
 							break
 						}
@@ -71,25 +71,24 @@ func (app *Application) StartStreams() {
 					for {
 						pkt, err := session.ReadPacket()
 						if err != nil {
-							log.Printf("Can't read session's packet %s (%s): %s\n", name, url, err.Error())
+							log.Printf("Can't read session's packet %s (%s): %s\n", streamID, url, err.Error())
 							break
 						}
-						err = app.castMSE(name, pkt)
+						err = app.castMSE(streamID, pkt)
 						if err != nil {
-							log.Printf("Can't cast packet %s (%s): %s\n", name, url, err.Error())
+							log.Printf("Can't cast packet %s (%s): %s\n", streamID, url, err.Error())
 							break
 						}
 					}
 				}
-
 				session.Close()
-				err = app.updateStatus(name, false)
+				err = app.updateStatus(streamID, false)
 				if err != nil {
-					log.Printf("Can't update status 'false' for %s (%s): %s\n", name, url, err.Error())
+					log.Printf("Can't update status 'false' for %s (%s): %s\n", streamID, url, err.Error())
 					time.Sleep(60 * time.Second)
 					continue
 				}
-				log.Printf("Stream must be re-establishment for '%s' by connecting to %s in next 5 seconds\n", name, url)
+				log.Printf("Stream must be re-establishment for '%s' by connecting to %s in next 5 seconds\n", streamID, url)
 				time.Sleep(5 * time.Second)
 			}
 		}(k, hlsEnabled, url)
