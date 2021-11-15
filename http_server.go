be421cba3b934d9601a5770af4aba66ef7717847
Dimitrii Lopanov <LdDl@users.noreply.github.com>
2021-11-15 22:03:35 +0300

    Update http_server.go

--- http_server.go
+++ http_server.go
@@ -16,6 +16,7 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// @todo: eliminate this regexp and use the third party
 var uuidRegExp = regexp.MustCompile("^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}")
 
 // StartVideoServer Initialize "video" server and run it (MSE-websockets and HLS-static files)
