6c1ba13680ac67a389b6beb535d4286b52d4be7d
Dimitrii <sexykdi@gmail.com>
2022-08-18 19:09:15 +0300

    split api / video

---
+++ video_server.go
@@ -0,0 +1,69 @@
+package videoserver
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/pprof"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+)
+
+// @todo: eliminate this regexp and use the third party
+var uuidRegExp = regexp.MustCompile("^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}")
+
+// StartVideoServer initializes "video" server and run it (MSE-websockets and HLS-static files)
+func (app *Application) StartVideoServer() {
+	router := gin.New()
+	// @todo I guess we should make proper configuration...
+	gin.SetMode(gin.ReleaseMode)
+	pprof.Register(router)
+
+	wsUpgrader := websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	if app.CorsConfig != nil {
+		router.Use(cors.New(*app.CorsConfig))
+	}
+	router.GET("/ws/:stream_id", WebSocketWrapper(app, &wsUpgrader))
+	router.GET("/hls/:file", HLSWrapper(app))
+	s := &http.Server{
+		Addr:         fmt.Sprintf("%s:%d", app.Server.HTTPAddr, app.Server.VideoHTTPPort),
+		Handler:      router,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+	err := s.ListenAndServe()
+	if err != nil {
+		log.Printf("Can't run Video-server on port: %d\n", app.Server.VideoHTTPPort)
+		return
+	}
+}
+
+// WebSocketWrapper returns WS handler
+func WebSocketWrapper(app *Application, wsUpgrader *websocket.Upgrader) func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+		wshandler(wsUpgrader, ctx.Writer, ctx.Request, app)
+	}
+}
+
+// HLSWrapper returns HLS handler (static files)
+func HLSWrapper(app *Application) func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+		file := ctx.Param("file")
+		_, err := uuid.Parse(uuidRegExp.FindString(file))
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+			return
+		}
+		ctx.Header("Cache-Control", "no-cache")
+		ctx.FileFromFS(file, http.Dir(app.HLS.Directory))
+	}
+}
