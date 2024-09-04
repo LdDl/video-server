cb185a797c814b562599213bc1ad9b27cb7d972c
LdDl <sexykdi@gmail.com>
2024-09-04 14:12:09 +0300

    log configuration

--- application.go
+++ application.go
@@ -2,7 +2,6 @@ package videoserver
 
 import (
 	"fmt"
-	"log"
 	"strings"
 
 	"github.com/LdDl/video-server/configuration"
@@ -11,11 +10,12 @@ import (
 
 	"github.com/deepch/vdk/av"
 	"github.com/google/uuid"
+	"github.com/rs/zerolog/log"
 )
 
 var (
-	ErrStreamTypeNotExists    = fmt.Errorf("Stream type does not exists")
-	ErrStreamTypeNotSupported = fmt.Errorf("Stream type is not supported")
+	ErrStreamTypeNotExists    = fmt.Errorf("stream type does not exists")
+	ErrStreamTypeNotSupported = fmt.Errorf("stream type is not supported")
 )
 
 // Application is a configuration parameters for application
@@ -84,7 +84,7 @@ func NewApplication(cfg *configuration.Configuration) (*Application, error) {
 	for _, rtspStream := range cfg.RTSPStreams {
 		validUUID, err := uuid.Parse(rtspStream.GUID)
 		if err != nil {
-			log.Printf("Not valid UUID: %s\n", rtspStream.GUID)
+			log.Error().Err(err).Str("scope", "configuration").Str("stream_id", rtspStream.GUID).Msg("Not valid UUID")
 			continue
 		}
 		outputTypes := make([]StreamType, 0, len(rtspStream.OutputTypes))
@@ -166,7 +166,6 @@ func (app *Application) addClient(streamID uuid.UUID) (uuid.UUID, chan av.Packet
 
 func (app *Application) clientDelete(streamID, clientID uuid.UUID) {
 	app.Streams.deleteClient(streamID, clientID)
-
 }
 
 func (app *Application) startHlsCast(streamID uuid.UUID, stopCast chan bool) {
