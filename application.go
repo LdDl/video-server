3df0db4b7a7d9447e48af40798e3623546cfd138
LdDl <sexykdi@gmail.com>
2024-09-16 16:24:03 +0300

    go func() args in startHlsCast

--- application.go
+++ application.go
@@ -212,7 +212,12 @@ func (app *Application) startHlsCast(streamID uuid.UUID, stopCast chan bool) err
 	if !ok {
 		return ErrStreamNotFound
 	}
-	go app.startHls(streamID, stream.hlsChanel, stopCast)
+	go func(id uuid.UUID, hlsChanel chan av.Packet, stop chan bool) {
+		err := app.startHls(id, hlsChanel, stop)
+		if err != nil {
+			log.Error().Err(err).Str("scope", "hls").Str("event", "hls_start_cast").Str("stream_id", id.String()).Msg("Error on HLS cast start")
+		}
+	}(streamID, stream.hlsChanel, stopCast)
 	return nil
 }
 
@@ -226,7 +231,7 @@ func (app *Application) startMP4Cast(streamID uuid.UUID, stopCast chan bool) err
 	go func(id uuid.UUID, mp4Chanel chan av.Packet, stop chan bool) {
 		err := app.startMP4(id, mp4Chanel, stop)
 		if err != nil {
-			log.Error().Err(err).Str("scope", "archive").Str("event", "archive_start_cast").Str("stream_id", id.String()).Msg("Error on cast start")
+			log.Error().Err(err).Str("scope", "archive").Str("event", "archive_start_cast").Str("stream_id", id.String()).Msg("Error on MP4 cast start")
 		}
 	}(streamID, stream.mp4Chanel, stopCast)
 	return nil
