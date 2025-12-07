c11ac9bdc674d6ef8b489655e34053b821d84648
Dimitrii Lopanov <sexykdi@gmail.com>
2025-12-07 13:40:19 +0300

    fix id in npm projects + open ssl legacy

--- example_client/hls_example/src/App.vue
+++ example_client/hls_example/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app" >
     <p>HLS player</p>
     <div>
-      <HLSPlayer schema="http" server="localhost" :port="8090" suuid="74f17aa1-dd57-4c77-96de-b71668a9a25a" :verbose="false" />
+      <HLSPlayer schema="http" server="localhost" :port="8090" suuid="13bf3e5c-d537-4f10-98e8-9d0afca8b070" :verbose="false" />
     </div>
   </div>
 </template>
